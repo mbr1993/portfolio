16709705d2acba1f350c71716d65d2e9135c71c3
mbr1993 <boburmannonov93@gmail.com>
2022-01-06 22:05:11 +0500

    installing npm

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//Route::resource('blog-tips', );
+require __DIR__.'/auth.php';
